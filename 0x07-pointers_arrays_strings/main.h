7b5dac5f7c2c477faa100cd3a3ac49707424fc11
favourokon <favourokon264@gmail.com>
2023-07-03 13:25:19 -0700

    0-commit

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_menset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *mencpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strbrk(char *s, char *accept);
+char *strstr(char *s char *accept);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a int size);
+void set_string(char **s, char *to);
+#endif
+}
